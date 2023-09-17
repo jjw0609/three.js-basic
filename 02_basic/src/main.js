51e6cecc9a2b009daec9d119d9eb3cf3f436f1f1
Jung JaeWoong <jjw_0609@naver.com>
2023-09-17 20:10:25 +0900

    feat: create camera

--- 02_basic/src/main.js
+++ 02_basic/src/main.js
@@ -8,3 +8,17 @@ import * as THREE from 'three';
 const canvas = document.querySelector('#three-canvas');
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+    75,     // 시야각(field of view)
+    window.innerWidth / window.innerHeight, // 종횡비(aspect)
+    0.1,    // near
+    1000    // far
+);
+camera.position.z = 5;
+scene.add(camera);
+
