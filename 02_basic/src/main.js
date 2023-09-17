f3bac029aba270852dda3aeb7487a84fe1464e4e
Jung JaeWoong <jjw_0609@naver.com>
2023-09-17 20:23:32 +0900

    feat: create mesh

--- 02_basic/src/main.js
+++ 02_basic/src/main.js
@@ -1,24 +1,51 @@
 import * as THREE from 'three';
 
-// 동적으로 캔버스 조립하기
-// const renderer = new THREE.WebGL1Renderer();
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
 
-const canvas = document.querySelector('#three-canvas');
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(window.innerWidth, window.innerHeight);
+export default function example() {
+    // Renderer
 
-// Scene
-const scene = new THREE.Scene();
+    // 동적으로 캔버스 조립하기
+    // const renderer = new THREE.WebGL1Renderer();
+    // renderer.setSize(window.innerWidth, window.innerHeight);
+    // document.body.appendChild(renderer.domElement);
 
-// Camera
-const camera = new THREE.PerspectiveCamera(
-    75,     // 시야각(field of view)
-    window.innerWidth / window.innerHeight, // 종횡비(aspect)
-    0.1,    // near
-    1000    // far
-);
-camera.position.z = 5;
-scene.add(camera);
+    // html에서 캔버스 가져와서 사용하기
+    const canvas = document.querySelector('#three-canvas');
+    // const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+    const renderer = new THREE.WebGLRenderer({
+        canvas,
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Scene
+    const scene = new THREE.Scene();
+
+    // Camera
+    const camera = new THREE.PerspectiveCamera(
+        75,     // 시야각(field of view)
+        window.innerWidth / window.innerHeight, // 종횡비(aspect)
+        0.1,    // near
+        1000    // far
+    );
+    camera.position.x = 1;
+    camera.position.y = 2;
+    camera.position.z = 5;
+    scene.add(camera);
+
+    // Mesh
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({
+        // color: 0xff0000
+        // color: '#ff0000'
+        color: 'red'
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    // 그리기
+    renderer.render(scene, camera);
+}
+
+example();
 
