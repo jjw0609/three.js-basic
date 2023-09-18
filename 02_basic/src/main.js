ec34f877a980a0c15d31044f8e6348cd20be8b00
Jung JaeWoong <jjw_0609@naver.com>
2023-09-18 20:58:43 +0900

    feat: add 직교 카메라(Orthographic Camera)

--- 02_basic/src/main.js
+++ 02_basic/src/main.js
@@ -22,15 +22,32 @@ export default function example() {
     const scene = new THREE.Scene();
 
     // Camera
-    const camera = new THREE.PerspectiveCamera(
-        75,     // 시야각(field of view)
-        window.innerWidth / window.innerHeight, // 종횡비(aspect)
-        0.1,    // near
-        1000    // far
+    // Perspective Camera(원근 카메라)
+    // const camera = new THREE.PerspectiveCamera(
+    //     75,     // 시야각(field of view)
+    //     window.innerWidth / window.innerHeight, // 종횡비(aspect)
+    //     0.1,    // near
+    //     1000    // far
+    // );
+    // camera.position.x = 1;
+    // camera.position.y = 2;
+    // camera.position.z = 5;
+
+    // Orthographic Camera(직교 카메라)
+    const camera = new THREE.OrthographicCamera(
+        -(window.innerWidth / window.innerHeight),  // left
+        window.innerWidth / window.innerHeight, // right
+        1,  // top
+        -1, // bottom
+        0.1,
+        1000
     );
     camera.position.x = 1;
     camera.position.y = 2;
     camera.position.z = 5;
+    camera.lookAt(0, 0, 0);
+    camera.zoom = 0.5;
+    camera.updateProjectionMatrix();
     scene.add(camera);
 
     // Mesh
