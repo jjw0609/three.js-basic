96f97b1e3f8752250ec70c8695e09ce287fb3ac3
Jung JaeWoong <jjw_0609@naver.com>
2023-12-14 21:36:55 +0900

    Line으로 선 만들고 Mesh 배치하기

---
+++ 09_raycaster/src/ex01.js
@@ -0,0 +1,84 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// ----- 주제: 특정 방향의 광선(Ray)에 맞은 Mesh 판별하기
+
+export default function example() {
+	// Renderer
+	const canvas = document.querySelector('#three-canvas');
+	const renderer = new THREE.WebGLRenderer({
+		canvas,
+		antialias: true
+	});
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+	// Scene
+	const scene = new THREE.Scene();
+
+	// Camera
+	const camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
+	camera.position.x = 5;
+	camera.position.y = 1.5;
+	camera.position.z = 4;
+	scene.add(camera);
+
+	// Light
+	const ambientLight = new THREE.AmbientLight('white', 0.5);
+	scene.add(ambientLight);
+
+	const directionalLight = new THREE.DirectionalLight('white', 1);
+	directionalLight.position.x = 1;
+	directionalLight.position.z = 2;
+	scene.add(directionalLight);
+
+	const controls = new OrbitControls(camera, renderer.domElement);
+
+	// Mesh
+	const lineMaterial = new THREE.LineBasicMaterial({color: 'yellow'});
+	const points = [];
+	points.push(new THREE.Vector3(0, 0, 100));
+	points.push(new THREE.Vector3(0, 0, -100));
+	const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+	const guide = new THREE.Line(lineGeometry, lineMaterial);
+	scene.add(guide);
+
+	const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+	const boxMaterial = new THREE.MeshStandardMaterial({color: 'plum'});
+	const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+
+	const torusGeometry = new THREE.TorusGeometry(2, 0.5, 16, 100);
+	const torusMaterial = new THREE.MeshStandardMaterial({color: 'lime'});
+	const torusMesh = new THREE.Mesh(torusGeometry, torusMaterial);
+
+	scene.add(boxMesh, torusMesh);
+
+	const meshes = [boxMesh, torusMesh];
+
+	// 그리기
+	const clock = new THREE.Clock();
+
+	function draw() {
+		const delta = clock.getDelta();
+
+		renderer.render(scene, camera);
+		renderer.setAnimationLoop(draw);
+	}
+
+	function setSize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		renderer.render(scene, camera);
+	}
+
+	// 이벤트
+	window.addEventListener('resize', setSize);
+
+	draw();
+}
