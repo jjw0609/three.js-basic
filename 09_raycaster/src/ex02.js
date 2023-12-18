0b2094afe00686a39708c0c26b45134d579e550c
Jung JaeWoong <jjw_0609@naver.com>
2023-12-18 21:11:14 +0900

    드래그 클릭 방지 모듈로 만들기

--- 09_raycaster/src/ex02.js
+++ 09_raycaster/src/ex02.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { PreventDragClick } from './PreventDragClick.js';
 
 // ----- 주제: 클릭한 Mesh 선택하기
 
@@ -75,7 +76,7 @@ export default function example() {
 	}
 
 	function checkIntersects() {
-		if(mouseMoved) return;
+		if(preventDragClick.mouseMoved) return;
 
 		raycaster.setFromCamera(mouse, camera);
 
@@ -107,26 +108,7 @@ export default function example() {
 		checkIntersects();
 	});
 
-	let mouseMoved;
-	let clickStartX;
-	let clickStartY;
-	let clickStartTime;
-	canvas.addEventListener('mousedown', e => {
-		clickStartX = e.clientX;
-		clickStartY = e.clientY;
-		clickStartTime = Date.now();
-	});
-	canvas.addEventListener('mouseup', e => {
-		const xGap = Math.abs(e.clientX - clickStartX);
-		const yGap = Math.abs(e.clientY - clickStartY);
-		const timeGap = Date.now() - clickStartTime;
-		
-		if(xGap > 5 || yGap > 5 || timeGap > 500) {
-			mouseMoved = true;
-		} else {
-			mouseMoved = false;
-		}
-	});
+	const preventDragClick = new PreventDragClick(canvas);
 
 	draw();
 }
