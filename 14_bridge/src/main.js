75f4ba5b45eb17eec75a548fa8acccb042a740e2
Jung JaeWoong <jjw_0609@naver.com>
2024-01-13 22:44:34 +0900

    조명 설치

--- 14_bridge/src/main.js
+++ 14_bridge/src/main.js
@@ -12,6 +12,8 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 // Scene은 common.js에서 생성
 cm1.scene.background = new THREE.Color(cm2.backgroundColor);
@@ -28,13 +30,20 @@ camera.position.z = 4;
 cm1.scene.add(camera);
 
 // Light
-const ambientLight = new THREE.AmbientLight('white', 0.5);
+const ambientLight = new THREE.AmbientLight(cm2.lightColor, 0.8);
 cm1.scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight('white', 1);
-directionalLight.position.x = 1;
-directionalLight.position.z = 2;
-cm1.scene.add(directionalLight);
+const spotLightDistance = 50;
+const spotLight1 = new THREE.SpotLight(cm2.lightColor, 1);
+spotLight1.castShadow = true;
+const spotLight2 = spotLight1.clone();
+const spotLight3 = spotLight1.clone();
+const spotLight4 = spotLight1.clone();
+spotLight1.position.set(-spotLightDistance, spotLightDistance, spotLightDistance);
+spotLight2.position.set(spotLightDistance, spotLightDistance, spotLightDistance);
+spotLight3.position.set(-spotLightDistance, spotLightDistance, -spotLightDistance);
+spotLight4.position.set(-spotLightDistance, spotLightDistance, -spotLightDistance);
+cm1.scene.add(spotLight1, spotLight2, spotLight3, spotLight4);
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
