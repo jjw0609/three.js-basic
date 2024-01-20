f45367bde544e21362ecce98b4983c70fb9fcb8d
Jung JaeWoong <jjw_0609@naver.com>
2024-01-20 22:45:35 +0900

    동작 조건 제어

--- 14_bridge/src/main.js
+++ 14_bridge/src/main.js
@@ -204,21 +204,34 @@ function checkIntersects() {
 		break;
 	}
 }
+
+let fail = false;
+let jumping = false;
 function checkClickedObject(mesh) {
 	if(mesh.name.indexOf('glass') >= 0) {
 		// 유리판을 클릭했을 때
+		if(jumping || fail) return;
+
 		if(mesh.step - 1 === cm2.step) {
+			jumping = true;
 			cm2.step++;
 			console.log(cm2.step);
 
 			switch(mesh.type) {
 				case 'normal':
 					console.log('normal!');
+					setTimeout(() => {
+						fail = true;
+					}, 700);
 					break;
 				case 'strong':
 					console.log('strong!');
 					break;
 			}
+
+			setTimeout(() => {
+				jumping = false;
+			}, 1000);
 
 			gsap.to(
 				player.cannonBody.position,
@@ -250,17 +263,26 @@ function draw() {
 	cm1.world.step(1/60, delta, 3);
 	objects.forEach(item => {
 		if(item.cannonBody) {
-			item.mesh.position.copy(item.cannonBody.position);
-			item.mesh.quaternion.copy(item.cannonBody.quaternion);
+			if(item.name === 'player') {
+				item.mesh.position.copy(item.cannonBody.position);
+				if(fail) item.mesh.quaternion.copy(item.cannonBody.quaternion);
+	
+				if(item.modelMesh) {
+					item.modelMesh.position.copy(item.cannonBody.position);
+					if(fail) item.modelMesh.quaternion.copy(item.cannonBody.quaternion);
+				}
+				item.modelMesh.position.y += 0.15;
+			} else {
+				item.mesh.position.copy(item.cannonBody.position);
+				item.mesh.quaternion.copy(item.cannonBody.quaternion);
 
-			if(item.modelMesh) {
-				item.modelMesh.position.copy(item.cannonBody.position);
-				item.modelMesh.quaternion.copy(item.cannonBody.quaternion);
-
-				if(item.name === 'player') {
-					item.modelMesh.position.y += 0.15;
+				if(item.modelMesh) {
+					item.modelMesh.position.copy(item.cannonBody.position);
+					item.modelMesh.quaternion.copy(item.cannonBody.quaternion);
 				}
 			}
+
+			
 		}
 	})
 
